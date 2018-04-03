8e37f5ed1241ed886e59ed033d06393658bfcea7
hobilin <paulibareyes@gmail.com>
2018-04-03 18:04:27 -0300

    agregando id a data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
-$.getJSON( "./documentos.json", function( response ) {
-  var data = response;
-  console.log(data)
-});
+var myData = data.Tramites;
+console.log(myData)
+
+
+var nombre = $("#nombre").val();
+var rut = $("#rut").val();
+var direccion = $("#direccion").val();
+var correo = $("#correo").val();
+var tramites = [$("tramites"), $("tramites2")]
+var database = firebase.database();
+var users = database.ref().child('users');
+var eachUser = users.child(rut);
+
+var data = {
+  nombre: nombre,
+  rut: rut,
+  correo: correo,
+  direccion: direccion,
+  tramites : tramites
+}
+    eachUser.push(data);
